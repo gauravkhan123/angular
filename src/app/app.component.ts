55d9457936930dff99068435e0ea2de12b143e87
Gaurav <gauravkhan123@outlook.com>
2021-07-31 16:34:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+  title = 'Angular';
+  getname()
+  {
+    return "My name is Gaurav";
+  }
+  obj = { make: 'Ford',
+  model: 'Mustang',
+    year: 1969
+}
+arr=['ram','shyam','laxman'];
+siteurl = window.location.href;
+//$array1 = array('key1', 'key2', 'key3'); $array2 = array('umesh', 'sonu', 'deepak');
+//$new_array = array_combine($array1, $array2); 
+//console.log($new_array);
+//print_r($new_array);
 }
